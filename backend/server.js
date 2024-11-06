beaaa7a2ba9330e29dce682b4476290ff62da392
ShubhamSehra <sehra.shubham11@gmail.com>
2024-11-07 00:47:11 +0530

    changing cors

--- backend/server.js
+++ backend/server.js
@@ -9,11 +9,11 @@ const app = express();
 
 
 const PORT = process.env.PORT || 5000
-app.use(cors())
-// app.use(cors({
-//   origin: 'https://thegreenfield.netlify.app',
-//   credentials: true
-// }));
+// app.use(cors())
+app.use(cors({
+  origin: 'https://thegreenfield.netlify.app',
+  credentials: true
+}));
 
 
 app.use(express.json());
--- frontend/src/components/Students.jsx
+++ frontend/src/components/Students.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import Card from "./Card";
 import "./background.css";
 import {  Form, InputGroup } from "react-bootstrap";
-
+import axios from "axios"
 import { useSearchParams } from "react-router-dom";
 
 function Students() {
@@ -10,34 +10,32 @@ function Students() {
   
   const [searchParams, setSearchParams] = useSearchParams();
 
-  // useEffect (() =>{
-  //   const getData = async() =>{
-  //     try {
-  //       const response = await axios.get(`${process.env.REACT_APP_API_URL}/students`)
-  //       setStudent(response.data)
-  //     } catch (error) {
-  //       console.log(error);
+  useEffect (() =>{
+    const getData = async() =>{
+      try {
+        const response = await axios.get(`${process.env.REACT_APP_API_URL}/students`)
         
-  //     }
-  //   }
-  //   getData()
-  // },[])
-  useEffect(() => { 
-    fetch('https://greenfield-lg3s.onrender.com/students',{
-      method: 'GET',
-      credentials: "include"
-    })
-    .then((res) => { 
-      if (!res.ok) { 
-        throw new Error(`HTTP error! status: ${res.status}`); 
-      } 
-      return res.json(); 
-    })
-    .then((jsonres) => setStudent(jsonres))
-    .catch((error) => {  
-      console.error('Error fetching students:', error); 
-    }); 
-  }, [])
+        setStudent(response.data)
+      } catch (error) {
+        console.log(error);
+        
+      }
+    }
+    getData()
+  },[])
+  // useEffect(() => { 
+  //   fetch(`${process.env.REACT_APP_API_URL}/api/students`)
+  //   .then((res) => { 
+  //     if (!res.ok) { 
+  //       throw new Error(`HTTP error! status: ${res.status}`); 
+  //     } 
+  //     return res.json(); 
+  //   })
+  //   .then((jsonres) => setStudent(jsonres))
+  //   .catch((error) => {  
+  //     console.error('Error fetching students:', error); 
+  //   }); 
+  // }, [])
 
   // useEffect(() => {
   //   fetch(`${process.env.REACT_APP_API_URL}/students`)
